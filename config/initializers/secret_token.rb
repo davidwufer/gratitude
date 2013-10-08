42501a4624cb6561b3551c8320c1fc35d3fed462
David Wu <davidcnwu@gmail.com>
2013-10-07 23:01:17 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gratitude::Application.config.secret_key_base = '9f0093ea6b12afc305ec37dfe10db4ce7c706c2ac8a4055b8ddca4613933c97b02f007b61780fa52f50300f713f3bebd1efddf5f14584b018d2f068c9619207b'
